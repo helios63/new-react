7c1464d043f63fbc807ae31999edd0563fa8bd53
Maxime Mondet <maxime.mondet@gmail.com>
2021-12-07 15:55:12 +0100

    First changes to build index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const images = "https://source.unsplash.com/random"
+const subject = ["tokyo", "paris", "barcelona", "london", "medellin", "berlin"]
+const theme = subject[Math.floor(Math.random() * subject.length)];
+
+const App = (props) => {
+
+  const [currentImage, setCurrentImage] = useState(0)
+
+  const handleClick = () => {
+    const length = images.length - 1
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="App">
+
+      <header className="title">
+        <h1>{theme}</h1>
+        <h2>A photography project<br/> by Unsplash</h2>
       </header>
-    </div>
+
+      <figure>
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        <img alt="" src={images + "/" + theme} onClick={handleClick} />
+      </figure>
+
+    </section>
   );
 }
 
